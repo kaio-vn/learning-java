25161cffbfa036774a2e0f3758d1e042865d84a1
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-01 16:45:05 -0300

    BeeCrowd 1009

---
+++ src/exercises/beginner/BeeCrowd1009.java
@@ -0,0 +1,20 @@
+package exercises.beginner;
+import java.util.Scanner;
+
+public class BeeCrowd1009 {
+    public static void main (String[] args){
+
+    Scanner sc = new Scanner (System.in);
+
+    String nome = sc.nextLine();
+    double salario = sc.nextDouble();
+    double totalVendas = sc.nextDouble();
+    double totalSalario = (totalVendas * 0.15) + salario;
+
+        System.out.printf("TOTAL = %.2f%n", totalSalario);
+
+
+    sc.close();
+
+    }
+}
