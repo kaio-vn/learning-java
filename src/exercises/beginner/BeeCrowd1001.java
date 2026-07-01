66b1f8a8fd36a4711b819efa3846c15990b28c86
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-01 15:41:00 -0300

    Add sum exercise: BeeCrowd 1001

---
+++ src/exercises/beginner/BeeCrowd1001.java
@@ -0,0 +1,19 @@
+package exercises.beginner;
+import java.util.Scanner;
+
+public class BeeCrowd1001 {
+    public static void main (String[] args){
+
+    Scanner sc = new Scanner (System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int soma = a + b;
+
+    System.out.println("X = " + soma);
+
+
+    sc.close();
+
+    }
+}
