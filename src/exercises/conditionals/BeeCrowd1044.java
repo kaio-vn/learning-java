1a94121b3893b81480e8feb9820507290a99c800
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-01 17:25:23 -0300

    BeeCrowd1044

---
+++ src/exercises/conditionals/BeeCrowd1044.java
@@ -0,0 +1,24 @@
+package exercises.conditionals;
+import java.util.Scanner;
+
+public class BeeCrowd1044 {
+    public static void main (String[] args){
+
+    Scanner sc = new Scanner (System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    if (A % B == 0 || B % A == 0 ){
+        System.out.println("São Múltiplos");
+    }
+    else {
+        System.out.println("Não são múltiplos");
+    }
+
+    sc.close();
+
+
+
+    }
+}
