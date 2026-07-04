0ff97b7e4e3fe4a42ac391826db69ebebf2eb3cb
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-04 20:08:47 -0300

    Weekdays

---
+++ src/exercises/conditionals/Weekdays.java
@@ -0,0 +1,41 @@
+package exercises.conditionals;
+import java.util.Scanner;
+
+public class Weekdays {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Input number: ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid number. Enter a value between 1 and 7.");
+        }
+
+        sc.close();
+
+    }
+}
