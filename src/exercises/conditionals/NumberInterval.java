c3807f425b161d98dd2482adb1d586ef19a6967f
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-06 19:28:08 -0300

    NumberInterval

--- src/exercises/conditionals/NumberInterval.java
+++ src/exercises/conditionals/NumberInterval.java
@@ -1,7 +1,7 @@
 package exercises.conditionals;
 import java.util.Scanner;
 
-public class BeeCrowd1037 {
+public class NumberInterval {
     public static void main (String[] args){
 
         Scanner sc = new Scanner (System.in);
