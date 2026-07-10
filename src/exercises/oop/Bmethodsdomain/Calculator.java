a779675ff04f3fd7b6e51d3b5ed75bf47f05aa98
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-10 14:18:09 -0300

    Calculator Method

---
+++ src/exercises/oop/Bmethodsdomain/Calculator.java
@@ -0,0 +1,11 @@
+package exercises.oop.Bmethodsdomain;
+
+public class Calculator {
+    public void twoNumbersSum(){
+        System.out.println(10 + 10);
+    }
+
+    public void twoNumbersMultiplication(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/exercises/oop/Bmethodsdotest/CalculatorTest.java
@@ -0,0 +1,13 @@
+package exercises.oop.Bmethodsdotest;
+
+import exercises.oop.Bmethodsdomain.Calculator;
+
+public class CalculatorTest {
+    static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.twoNumbersMultiplication(20, 10);
+
+    }
+}
